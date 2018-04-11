1f3fcf7200aa7d0da3a6fcfd55674a6e06b2d03f
GustavoCostella <31291669+GustavoCostella@users.noreply.github.com>
2018-04-11 16:32:02 -0300

    Add files via upload
    Trabalho 2 de algoritmos

---
+++ trab2.py
@@ -0,0 +1,22 @@
+#Programa em python que controle a retirada de dinheiro de um cofre.
+#Gustavo Costella Barbosa e-mail: gustavocostellabarbosa@hotmail.com // github.com/GustavoCostella
+import os
+nota5 = 0 
+nota10 = 0
+nota20 = 0
+val_retirada = int(input("Digite o valor de retirada"))
+
+
+while True:
+print("----------Bem vindo ao banco!----------")
+print("----------Operaçoes disponiveis:----------")
+print("----------Retirada----------")
+	
+
+
+
+
+
+
+if valor_retirada == 0:
+	exit() 
